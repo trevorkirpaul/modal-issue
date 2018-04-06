a7c74732d7be5d42c96ce102b63c94d9ee5f7f56
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-04-05 21:57:11 -0400

    init com

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Modal, Button, Alert} from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+    this.handleToggleModal = this.handleToggleModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+  handleToggleModal() {
+    this.setState({ open: !this.state.open })
+  }
+  handleCloseModal() {
+    this.setState({ open: false })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Open Modal"
+          onPress={() => this.handleToggleModal()}
+        />
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.open}
+
+        >
+          <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', backgroundColor: 'palevioletred' }}>
+            <Button
+              title="Open Alert"
+              color="white"
+              onPress={() => Alert.alert('Alert', 'Close modal', [{text: 'Ok', onPress: () => this.handleCloseModal()}])}
+            />
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
